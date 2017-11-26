413d0c592b75b131bd3df643073a03b355cae8f4
candy <1915998056@qq.com>
2017-11-26 09:19:53 +0800

    增加打卡相关的注释

--- Dota2_MLM_WX/src/main/java/org/dota2school/mlm/wx/controller/MlmWxClockController.java
+++ Dota2_MLM_WX/src/main/java/org/dota2school/mlm/wx/controller/MlmWxClockController.java
@@ -5,24 +5,69 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 打卡记录的controller
+ * @author Xuxue1 2017-11-26
+ */
 @RestController
 @RequestMapping("/api/mlm/wx/clock")
 public class MlmWxClockController {
 
+    /**
+     *
+     * @param session
+     * @param classType
+     * @param students
+     * @param teachTime
+     * @param teachDate
+     * @return
+     */
     @RequestMapping(value = "/add",method = RequestMethod.POST)
-    public Entry addClock(){
+    public Entry addClock(
+            String session,
+            String classType,
+            String students,
+            String teachTime,
+            String teachDate){
         return null;
     }
 
+    /**
+     *
+     * @param session
+     * @param signId
+     * @return
+     */
     @RequestMapping(value = "/get",method = RequestMethod.GET)
-    public Entry getClock(){
+    public Entry getClock(
+            String session,
+            String signId){
 
         return null;
     }
 
+    /**
+     *
+     * @param seesion
+     * @param iv
+     * @param code
+     * @param encryptedData
+     * @param signId
+     * @param status
+     * @param evaluate
+     * @param pictrues
+     * @return
+     */
     @RequestMapping(value = "/response",method = RequestMethod.POST)
-    public Entry responseClock(){
-
+    public Entry responseClock(
+            String seesion,
+            String iv,
+            String code,
+            String encryptedData,
+            String signId,
+            String status,
+            String evaluate,
+            String pictrues){
         return null;
     }
 }
