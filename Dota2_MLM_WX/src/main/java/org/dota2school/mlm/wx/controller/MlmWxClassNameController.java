1fce6a84e2437353f9e80699a2ada9534695c6e0
candy <1915998056@qq.com>
2017-11-26 09:31:45 +0800

    增加班级名称相关的注释

--- Dota2_MLM_WX/src/main/java/org/dota2school/mlm/wx/controller/MlmWxClassNameController.java
+++ Dota2_MLM_WX/src/main/java/org/dota2school/mlm/wx/controller/MlmWxClassNameController.java
@@ -1,4 +1,25 @@
 package org.dota2school.mlm.wx.controller;
 
+
+import org.dota2school.mlm.common.model.Entry;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 获取班级的名称
+ * @author Xuxue1 2017-11-26
+ */
+@RestController
+@RequestMapping("/api/mlm/wx/class_name")
 public class MlmWxClassNameController {
+
+    /**
+     * 查询班级名称
+     * @return
+     */
+    @RequestMapping("query")
+    public Entry queryClassName(){
+        return null;
+    }
+
 }
